e3f51d66cbb06b6ced0a848493d68bd5e07ffbf8
lucianohcl <lucianonetexame21@gmail.com>
2026-04-19 13:02:38 -0300

    fix: adicionado import do plotly para corrigir NameError no grafico

--- analise_de_tarefas.py
+++ analise_de_tarefas.py
@@ -29,6 +29,8 @@ from docx.shared import Pt, RGBColor
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 from fpdf import FPDF
 import pandas as pd
+import plotly.graph_objects as go
+import plotly.express as px
 
 
 # ============================================================
